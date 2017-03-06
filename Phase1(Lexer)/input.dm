7b77ee5a2f1e1c9d6b42142bfceb4c392d8d6c3e
Danial <danial.alihosseini@gmail.com>
2017-03-06 16:58:07 +0330

    all rules are written. a test case added. it has errors in true false.

---
+++ Phase1(Lexer)/input.dm
@@ -0,0 +1,8 @@
+program example
+    int i := #1;
+    int j := #2;
+    int k := #3;
+    real r := #1.1;
+    boolean b := true;
+    int array[#2] := {#1, #7};
+    char chars[i..k] := {'c', 'd', '7'};
--- Phase1(Lexer)/pylex.py
+++ Phase1(Lexer)/pylex.py
@@ -39,35 +39,108 @@ reserved = {
 
 # List of token names.   This is always required
 tokens = [
-             'ID', 'NUMCONST', 'CHARCONST', 'WHITE_SPACE', 'COMMENTS'
+             'ID', 'NUMCONST', 'REALCONST', 'BOOLCONST',
+             'CHARCONST', 'WHITE_SPACE', 'COMMENTS', 'NEWLINE',
+             'LT', 'LE', 'GT', 'GE', 'EQ', 'NEQ', 'LPAR', 'RPAR', 'LBRACK', 'RBRACK', 'LBRACE', 'RBRACE',
+             'PLUS', 'MINUS', 'MULT', 'DIV', 'MOD', 'ASSIGNMENT_SIGN',
+             'SEMICOLON', 'DOUBLE_DOT', 'COMMA', 'COLON'
          ] + list(reserved.values())
 
 letter = r'([a-zA-Z])'
+zero = r'(0)'
+non_zero_digit = r'([1-9])'
 digit = r'([0-9])'
 identifier = r'(' + letter + r'+)'
+backslash_charconst = r'(\\(.))'
+quoted_charconst = r'(\'(.)\')'
+charconst = r'(' + backslash_charconst + '|' + quoted_charconst + r')'
+numconst = r'(\#' + non_zero_digit + digit + r'*|' + zero + r')'
+realconst = r'(\#(' + non_zero_digit + digit + r'*|' + zero + r')\.' +\
+            digit + r'*' + non_zero_digit + r')'
+boolconst = r'((true)|(false))'
+
+t_ignore = ' \t'
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'>='
+t_EQ = r'=='
+t_NEQ = r'<>'
+t_LPAR = r'\('
+t_RPAR = r'\)'
+t_LBRACK = r'\['
+t_RBRACK = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULT = r'\*'
+t_DIV = r'\\'
+t_MOD = r'%'
+t_ASSIGNMENT_SIGN = r':='
+t_SEMICOLON = r';'
+t_DOUBLE_DOT = r'\.\.'
+t_COLON = r'\:'
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    return t
+
 
 @TOKEN(identifier)
 def t_ID(t):
     t.type = reserved.get(t.value, 'ID')  # Check for reserved words
     return t
 
-numconst = r'(#' + digit + r'+(\.' + digit + r'+)?)'
+
+@TOKEN(realconst)
+def t_REALCONST(t):
+    return t
+
 
 @TOKEN(numconst)
 def t_NUMCONST(t):
     return t
 
-backslash_const_char = r'(\\(.))'
-quoted_const_char = r''
-char_const = r'(' + backslash_const_char + '|' + quoted_const_char + r')'
 
+@TOKEN(charconst)
 def t_CHARCONST(t):
+    if t.value == "\\0":
+        t.character = None
+    else:
+        t.character = t.value[1:]
     return t
 
 
-def t_WHITE_SPACE(t):
+@TOKEN(boolconst)
+def t_BOOLCONST(t):
     return t
 
 
 def t_COMMENTS(t):
-    return t
+    r'//.*'
+    return
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# read input file
+code = None
+with open('./input.dm', 'r') as input_file:
+    code = input_file.read()
+# Give the lexer some input
+lexer.input(code)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
